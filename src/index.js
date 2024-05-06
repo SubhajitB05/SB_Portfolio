b4e83a3caff7ba09eabdf1d3849590d14ba8540a
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2024-05-06 19:31:52 +0530

    fixed routing error

--- src/index.js
+++ src/index.js
@@ -1,86 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-// import reportWebVitals from './reportWebVitals';
-import ErrorPage from "./Error";
-import Home from "./Home/Home";
-import About from "./About/About";
-import Education from "./Education/Education";
-import CodingProfiles from "./Coding Profiles/CodingProfiles";
-import Projects from "./Projects/Projects";
-import Contact from "./Contact/Contact";
-
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-import Layout from "./Layout/Layout";
-// import { AnimatePresence } from "framer-motion";
-
-const router = createBrowserRouter([
-  {
-    path: "/" || "/home",
-    element: (
-      <>
-        <Layout />
-        <Home />
-      </>
-    ),
-    errorElement: <ErrorPage />,
-  },
-
-  {
-    path: "/about",
-    element: (
-      <>
-        <Layout />
-        <About />
-      </>
-    ),
-  },
-  {
-    path: "/education",
-    element: (
-      <>
-        <Layout />
-        <Education />
-      </>
-    ),
-  },
-  {
-    path: "/projects",
-    element: (
-      <>
-        <Layout />
-        <Projects />
-      </>
-    ),
-  },
-  {
-    path: "/coding-profiles",
-    element: (
-      <>
-        <Layout />
-        <CodingProfiles />
-      </>
-    ),
-  },
-  {
-    path: "/contact",
-    element: (
-      <>
-        <Layout />
-        <Contact />
-      </>
-    ),
-  },
-]);
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
